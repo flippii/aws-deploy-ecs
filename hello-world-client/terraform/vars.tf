669b486c13512765ac8f6df3036859cdc05b0aea
flippii <Knut.Genthe@gmx.de>
2025-08-22 15:32:41 +0200

    update subnet tag names

--- hello-world-client/terraform/vars.tf
+++ hello-world-client/terraform/vars.tf
@@ -1,22 +1,18 @@
 variable "region" {
-  default = "us-east-1"
+  default = "eu-north-1"
 }
-
 variable "environment" {
   default = "dev"
 }
 variable "cloudmap_namespace"{
-  default =""
+  default = ""
 }
-
 variable "application" {
   default = "client"
 }
-
 variable "vpc_id" {
   default = ""
 }
-
 variable "container_version" {
   default = ""
 }
--- world-service/terraform/vars.tf
+++ world-service/terraform/vars.tf
@@ -1,22 +1,18 @@
 variable "region" {
-  default = "us-east-1"
+  default = "eu-north-1"
 }
-
 variable "environment" {
   default = "dev"
 }
 variable "cloudmap_namespace"{
   default =""
 }
-
 variable "application" {
   default = "world"
 }
-
 variable "vpc_id" {
   default = ""
 }
-
 variable "container_version" {
   default = ""
 }
